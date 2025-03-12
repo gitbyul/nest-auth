a79f8e5bb8a6185301f9a0c9c249a36307752342
gitbyul <gitbyul@gmail.com>
2025-03-12 14:01:05 +0900

    app struct init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './domain/app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+
+  const port = process.env.PORT ?? 3000;
+  const node = process.env.NODE_ENV;
+  const db_host = process.env.DB_HOST;
+  await app.listen(port, () => {
+    logger.verbose(`Application is running env: ${node}`);
+    logger.verbose(`Application is running url: http://localhost:${port}`);
+    logger.verbose(`Database is running host: ${db_host}`);
+  });
 }
 bootstrap();
