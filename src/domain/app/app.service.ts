e52fb34901ea5eab7ad210b01a9aa2044e91830f
gitbyul <gitbyul@gmail.com>
2025-03-13 16:05:28 +0900

    feature: get & post test API

--- src/domain/app/app.service.ts
+++ src/domain/app/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getGetHello(): string {
+    return 'Get Hello World!';
+  }
+
+  getPostHello(): string {
+    return 'Post Hello World!';
   }
 }
