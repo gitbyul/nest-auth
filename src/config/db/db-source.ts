036dc94128bd491fd25967bb20f12987e906b2c5
gitbyul <gitbyul@gmail.com>
2025-03-13 15:47:20 +0900

    feature: localhost db sync 용 DataSource 작성 [npm run db:sync] 참고

---
+++ src/config/db/db-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export const AppDataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'dev_auth',
+  synchronize: false,
+  entities: ['dist/**/*.entity{.ts,.js}'], // 엔티티 파일 경로 설정
+  namingStrategy: new SnakeNamingStrategy(),
+});
