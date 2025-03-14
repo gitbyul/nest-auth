8665cb270a415cd8e623b2da3741ace385175da0
gitbyul <gitbyul@gmail.com>
2025-03-14 11:47:06 +0900

    feature: [AppModule] Reponse Interceptor

--- src/domain/app/app.module.ts
+++ src/domain/app/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ResponseInterceptor } from 'src/common/interceptor/response.intercepetor';
 import { DBModule } from 'src/config/db/db.module';
 import { EnvModule } from 'src/config/env/env.module';
 import { LogModule } from 'src/config/log/log.module';
@@ -8,6 +10,9 @@ import { AppService } from './app.service';
 @Module({
   imports: [EnvModule, DBModule, LogModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_INTERCEPTOR, useClass: ResponseInterceptor },
+  ],
 })
 export class AppModule {}
